0ef6d984b4c44f665adb221b0bf690b1599325ba
Melanie Burger <melanie.brgr@gmail.com>
2016-05-08 16:52:08 -0400

    data bind cost calculations

---
+++ js/app.js
@@ -0,0 +1,23 @@
+angular.module('myApp', [])
+	.controller('myController', function($scope) {
+		$scope.charges_data = {
+			subtotal: 0,
+			tip: 0,
+			total: 0
+		};
+		$scope.master_data = {
+			tipTotal: 0,
+			mealCount: 0,
+			avgTips: 0
+		};
+
+		$scope.submit = function() {
+			$scope.charges_data.subtotal = $scope.meal_data.baseMealPrice * (1+($scope.meal_data.taxRate/100));
+			$scope.charges_data.tip = $scope.charges_data.subtotal * ($scope.meal_data.tipPercentage/100);
+			$scope.charges_data.total = $scope.charges_data.subtotal + $scope.charges_data.tip;
+			
+			$scope.master_data.tipTotal += $scope.charges_data.tip;
+			$scope.master_data.mealCount += 1;
+			$scope.master_data.avgTips = $scope.master_data.tipTotal/$scope.master_data.mealCount;
+		};
+	});
