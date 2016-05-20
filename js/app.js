3637c52916e0149ae04202bf6de8a93ad59f353d
Melanie Burger <melanie.brgr@gmail.com>
2016-05-20 07:46:53 -0400

    one controller per view

--- js/app.js
+++ js/app.js
@@ -2,55 +2,65 @@ angular.module('myApp', ['ngMessages', 'ngRoute'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl: 'home.html',
-			controller: 'myController'
+			controller: 'homeCtrl'
 		})
 		.when('/newmeal', {
 			templateUrl: 'newmeal.html',
-			controller: 'myController'
+			controller: 'mealCtrl'
 		})
 		.when('/myearnings', {
 			templateUrl: 'myearnings.html',
-			controller: 'myController'
+			controller: 'earningsCtrl'
 		})
 	}])
-	.controller('myController', function($scope) {
-		$scope.charges_data = {
+	.controller('homeCtrl', function($scope) {
+		//Nothing here yet.
+	})
+	.controller('mealCtrl', function($scope, $rootScope) {
+		$rootScope.charges_data = {
 			subtotal: 0,
 			tip: 0,
 			total: 0
 		};
-		$scope.master_data = {
+		$rootScope.master_data = {
 			tipTotal: 0,
 			mealCount: 0,
 			avgTips: 0
 		};
 
 		$scope.submit = function() {
-			$scope.charges_data.subtotal = $scope.meal_data.baseMealPrice * (1+($scope.meal_data.taxRate/100));
-			$scope.charges_data.tip = $scope.charges_data.subtotal * ($scope.meal_data.tipPercentage/100);
-			$scope.charges_data.total = $scope.charges_data.subtotal + $scope.charges_data.tip;
+			$rootScope.charges_data.subtotal = $scope.meal_data.baseMealPrice * (1+($scope.meal_data.taxRate/100));
+			$rootScope.charges_data.tip = $scope.charges_data.subtotal * ($scope.meal_data.tipPercentage/100);
+			$rootScope.charges_data.total = $scope.charges_data.subtotal + $scope.charges_data.tip;
 			
-			$scope.master_data.tipTotal += $scope.charges_data.tip;
-			$scope.master_data.mealCount += 1;
-			$scope.master_data.avgTips = $scope.master_data.tipTotal/$scope.master_data.mealCount;
-			$scope.cancel();
+			$rootScope.master_data.tipTotal += $scope.charges_data.tip;
+			$rootScope.master_data.mealCount += 1;
+			$rootScope.master_data.avgTips = $scope.master_data.tipTotal/$scope.master_data.mealCount;
+			console.log('submit', $rootScope.charges_data);
+			console.log('submit', $rootScope.charges_data);
+			$rootScope.clearMeal();
 		};
 
-		$scope.cancel = function() {
+		$rootScope.clearMeal = function() {
 			var form = document.getElementsByName('mealDetails__form')[0],
 				inputs = Array.from(form.getElementsByTagName('input'));
 			inputs.forEach(function(el) {
 				el.value = '';
 			});
-			for (var data in $scope.charges_data) {
-				$scope.charges_data[data] = 0;
-			}
 		};
 
+		$rootScope.clearCustCharges = function() {
+			for (var data in $rootScope.charges_data) {
+				$rootScope.charges_data[data] = 0;
+			}			
+		}
+	})
+	.controller('earningsCtrl', function($scope, $rootScope) {
 		$scope.reset = function() {
-			$scope.cancel();
-			for (var data in $scope.master_data) {
-				$scope.master_data[data] = 0;
+			$rootScope.clearMeal();
+			$rootScope.clearCustCharges();
+			for (var data in $rootScope.master_data) {
+				$rootScope.master_data[data] = 0;
 			}
 		};
 	});
